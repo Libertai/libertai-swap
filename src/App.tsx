096a13fd7f32423c66b57f77c51181727c340099
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-10-16 03:15:18 +0900

    feat: Working testnet version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import WormholeConnect, {nttManualRoute} from '@wormhole-foundation/wormhole-connect';
+import WormholeConnect, {nttRoutes} from '@wormhole-foundation/wormhole-connect';
 
 function App() {
     return (
         <WormholeConnect config={{
             network: "Testnet",
-            routes: [nttManualRoute({
-                tokens: {
-                    LTAI: [{
-                        chain: "BaseSepolia",
-                        token: "0x92e1d72210429Ce7eE8a0d64D526D4b9752801FF",
-                        manager: "0xc16b38Ff76393df09D917F653b24EEA7A3cbafdE",
-                        transceiver: [{address: "0x58dC1D987Cb925E778266621Ca8f95c39c37E948", type: 'wormhole'}]
-                    }, {
-                        chain: "Solana",
-                        token: "5ooRTqmvm95bHwp4HMo3iszCwsEseY5hksfSa9EDVpBY",
-                        manager: "NTtAhS2BEN2Ek9JU7SfwLPu4MUTWVCUGkwk7DVrGya6",
-                        transceiver: [{address: "6boPo5aXJLof2brUi3REN2RjXMPj29FSekEK9iz8L4zB", type: "wormhole"}]
-                    }]
-                }
-            })],
+            routes: [
+                ...nttRoutes({
+                    tokens: {
+                        LTAI: [{
+                            chain: "BaseSepolia",
+                            token: "0x92e1d72210429Ce7eE8a0d64D526D4b9752801FF",
+                            manager: "0xc16b38Ff76393df09D917F653b24EEA7A3cbafdE",
+                            transceiver: [{address: "0x58dC1D987Cb925E778266621Ca8f95c39c37E948", type: 'wormhole'}]
+                        }, {
+                            chain: "Solana",
+                            token: "5ooRTqmvm95bHwp4HMo3iszCwsEseY5hksfSa9EDVpBY",
+                            manager: "NTtAhS2BEN2Ek9JU7SfwLPu4MUTWVCUGkwk7DVrGya6",
+                            transceiver: [{address: "6boPo5aXJLof2brUi3REN2RjXMPj29FSekEK9iz8L4zB", type: "wormhole"}]
+                        }]
+                    },
+                }),
+            ],
 
             chains: ["BaseSepolia", "Solana"],
             rpcs: {
@@ -50,26 +52,9 @@ function App() {
                     coinGeckoId: "libertai",
                     decimals: 9
                 }
-            }
+            },
         }}/>
     );
 }
 
 export default App
-
-
-// routes: nttRoutes({
-//     tokens: {
-//         LTAI: [{
-//             chain: "BaseSepolia",
-//             token: "LTAI",
-//             manager: "0xc16b38Ff76393df09D917F653b24EEA7A3cbafdE",
-//             transceiver: [{address: "0x58dC1D987Cb925E778266621Ca8f95c39c37E948", type: 'wormhole'}]
-//         }, {
-//             chain: "Solana",
-//             token: "LTAI",
-//             manager: "NTtAhS2BEN2Ek9JU7SfwLPu4MUTWVCUGkwk7DVrGya6",
-//             transceiver: [{address: "6boPo5aXJLof2brUi3REN2RjXMPj29FSekEK9iz8L4zB", type: "wormhole"}]
-//         }]
-//     }
-// }),
